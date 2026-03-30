15a4972d4a27fcac7fa5d5dce2bab9ade4668702
Foyjur Rafee <frnaib9@gmail.com>
2026-03-31 01:37:57 +0600

    shop page updated

--- js/script.js
+++ js/script.js
@@ -67,23 +67,44 @@ $(document).ready(function () {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 676,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1
-        },
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
       }
     ]
   });
